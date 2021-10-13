7cc602d08c025262c1b7b03b71aa1e857c0b0bf1
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-14 04:55:05 +0700

    Update hinata.sh

--- hinata.sh
+++ hinata.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-korupsi=$(echo "$(curl -s ifconfig.me)" | tr . k )
+sasuke=$(echo "$(curl -s ifconfig.me)" | tr . s )
 if [ -z "$STY" ]; then exec screen -dm -S $sasuke /bin/bash "$0"; fi
 chmod +x $sasuke.sh
 ./$sasuke.sh
