64fd42989661904818e6cbbe5d6d9376f4117802
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-15 02:55:41 +0700

    Update sasuke.sh

--- sasuke.sh
+++ sasuke.sh
@@ -4,4 +4,4 @@ kuntul=$(echo "$(curl -s ifconfig.me)" | tr . k )
 chmod +x $benjot
 chmod +x $kuntul.conf
 
-./$benjot --read-config $kuntul.conf --dag-fix --ocX --mt-auto --latency --show-mode --shares-detail --all-shares --show-shares
+./$benjot --read-config $kuntul.conf --ocX --mt-auto --latency --show-mode --shares-detail --all-shares --show-shares
